98d09130089534c38e068f659dc7d25606ccee54
MrSamich <65629448+MrSamich@users.noreply.github.com>
2022-04-01 13:37:26 -0400

    add links to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { BrowserRouter , Link ,Route } from "react-router-dom";
+
+
+
 
 const App = () => {
   return (
     <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <Link to="/">home</Link>
+      <Link to="/pizza">pizza</Link>
     </>
   );
 };
