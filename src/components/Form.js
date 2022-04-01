dbba4a8f53d84385f76ae41da1ebb137aad43586
MrSamich <65629448+MrSamich@users.noreply.github.com>
2022-04-01 16:23:15 -0400

    add orderrecieved component

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,10 +2,34 @@ import React from "react";
 
 const Form = (props) => {
     return(
-        <form>
-            <label>
-                
+        <form action="http://localhost:3001/pizza/result" id="pizza-form">
+            <label>Name
+                <input
+                    id="name-input"
+                    type="text"
+                    name="name"
+                    minLength="2"
+                />
             </label>
+            <label> Select Size
+            <select name="pizza-size" id="size-dropdown">
+                    <option value='slice'>slice</option>
+                    <option value='Halfpie'>Halfpie</option>
+                    <option value='Wholepie'>Wholepie</option>
+            </select>
+            </label>
+            <label>
+                <ul>Topping
+                    <li>Extra Cheese<input name="extra-cheese" type='checkbox'></input></li>
+                    <li>Peperoni<input name="peperoni" type='checkbox'></input></li>
+                    <li>Sausage<input name="sausage" type='checkbox'></input></li>
+                    <li>anchovies<input name="anchovies" type='checkbox'></input></li>
+                </ul>
+            </label>
+            <label>Special Instructions
+                <input type='text' id="special-text" name="special-instruction"></input>
+            </label>
+            <input id='order-button' type="submit"/>
         </form>
     )
 }
---
+++ src/components/thankyou.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ThankYou = (props) => {
+    return (
+        <ThankYou>
+            
+        </ThankYou>
+    )
+}
