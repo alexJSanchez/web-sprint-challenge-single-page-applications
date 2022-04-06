28030ef150d36c2ea5cf773306ec829a9a1791cc
MrSamich <65629448+MrSamich@users.noreply.github.com>
2022-04-06 12:55:35 -0400

    final fix

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,37 +1,52 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
+import * as yup from "yup"
 const Form = (props) => {
-    
-    
+    const [form, setForm] = useState({ name: '',})
+    const [error, setError] = useState({ name: '',})
+    const schema = yup.object().shape({
+        name: yup.string().min(2, 'name must be at least 2 characters'),
+    })
+
+    const setFormErrors = (name, value) =>{
+        yup.reach(schema, name).validate(value)
+        .then(() => setError({...error, [name]: ''}))
+        .catch(err => setError({...error, [name]: err.errors[0]}))
+    }
+
+    const change = event => {
+        const {checked, value ,name, type} = event.target
+        const valueToUse = type === "checkbox" ? checked : value;
+        setFormErrors(name, valueToUse)
+        setForm({...form, [name]: valueToUse})
+    }
     return(
         <form id="pizza-form">
             <label>Name
-                <input
+                 <input
+                    onChange={change}
                     id="name-input"
                     type="text"
                     name="name"
-                    minLength="2"
                 />
             </label>
+            <h1>{error.name}</h1>
             <label> Select Size
-            <select name="pizza-size" id="size-dropdown">
+            <select name="pizzaSize" id="size-dropdown">
                     <option value='slice'>slice</option>
                     <option value='Halfpie'>Halfpie</option>
                     <option value='Wholepie'>Wholepie</option>
             </select>
             </label>
-            <label>
                 <ul>Topping
-                    <li>Extra Cheese<input name="extra-cheese" type='checkbox'></input></li>
-                    <li>Peperoni<input name="peperoni" type='checkbox'></input></li>
-                    <li>Sausage<input name="sausage" type='checkbox'></input></li>
-                    <li>anchovies<input name="anchovies" type='checkbox'></input></li>
+                    <li>Extra Cheese<input value="extraCheese" name="toppings" type='checkbox'></input></li>
+                    <li>Pepperoni<input value="pepperoni" name="toppings" type='checkbox'></input></li>
+                    <li>Sausage<input value="sausage" name="toppings" type='checkbox'></input></li>
+                    <li>anchovies<input value="anchovies" name="toppings" type='checkbox'></input></li>
                 </ul>
-            </label>
             <label>Special Instructions
-                <input type='text' id="special-text" name="special-instruction"></input>
+                <input  type='text' id="special-text" name="specialInstruction"></input>
             </label>
-            <input id='order-button' type="submit"/>
+           <input id='order-button' type="submit"/>
         </form>
     )
 }
